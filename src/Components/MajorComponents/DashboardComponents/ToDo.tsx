9f27578c60188ee5c7f4deb3709b9d20226160fc
Movicks <codlabx@gmail.com>
2025-04-23 21:12:46 +0100

    dashboard 3

--- src/Components/MajorComponents/DashboardComponents/ToDo.tsx
+++ src/Components/MajorComponents/DashboardComponents/ToDo.tsx
@@ -6,7 +6,7 @@ export default function ToDo() {
     <div className="border border-gray-300 rounded-lg p-4 max-h-[380px] h-full">
         <div className="flex items-center justify-between border-b border-gray-300 pb-4">
             <div className="flex items-center gap-2">
-                <img src={Note} alt='icon' />
+                <img src={Note} alt='icon' loading='lazy'/>
                 <h1 className='text-sm font-semibold'>To-Do</h1>
             </div>
             <button className="filter-btn border-1 border-gray-300 bg-white px-3 py-1 rounded-lg text-gray-400">See All</button>
--- src/Components/MajorComponents/DashboardComponents/WorkHour.tsx
+++ src/Components/MajorComponents/DashboardComponents/WorkHour.tsx
@@ -6,7 +6,7 @@ export default function WorkHour() {
     <div className="border border-gray-300 rounded-lg p-4">
         <div className="flex items-center justify-between border-b border-gray-300 pb-4">
             <div className="flex items-center gap-2">
-                <img src={Clock} alt='icon' />
+                <img src={Clock} alt='icon' loading='lazy'/>
                 <h1 className='text-sm font-semibold'>Today`s Work Hour</h1>
             </div>
             <button className="filter-btn border-1 border-gray-300 bg-white px-3 py-1 rounded-lg text-gray-400">Details</button>
