be778b5367a65adc67888e8ba3d07dfd50f07b3e
Movicks <codlabx@gmail.com>
2025-04-23 21:18:33 +0100

    bug fixing

--- src/Components/MajorComponents/DashboardComponents/WorkHour.tsx
+++ src/Components/MajorComponents/DashboardComponents/WorkHour.tsx
@@ -1,12 +1,11 @@
 // import React from 'react'
-import Clock from '../../../Assets/icons/clock.svg';
 
 export default function WorkHour() {
   return (
     <div className="border border-gray-300 rounded-lg p-4">
         <div className="flex items-center justify-between border-b border-gray-300 pb-4">
             <div className="flex items-center gap-2">
-                <img src={Clock} alt='icon' loading='lazy'/>
+                <img src='Svgs/clock.svg' alt='icon' loading='lazy'/>
                 <h1 className='text-sm font-semibold'>Today`s Work Hour</h1>
             </div>
             <button className="filter-btn border-1 border-gray-300 bg-white px-3 py-1 rounded-lg text-gray-400">Details</button>
